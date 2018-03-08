066509b79dc142ff5abcb7ef06229290be1e0f0c
Kamil Biernacki <kbiernacki67@gmail.com>
2018-03-08 15:01:05 +0100

    com

--- src/main/java/pl/kamilbiernacki/springWeather/controllers/WeatherController.java
+++ src/main/java/pl/kamilbiernacki/springWeather/controllers/WeatherController.java
@@ -3,24 +3,31 @@ package pl.kamilbiernacki.springWeather.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import pl.kamilbiernacki.springWeather.models.WeatherModel;
+
+import org.springframework.web.bind.annotation.ResponseBody;
 import pl.kamilbiernacki.springWeather.models.services.WeatherService;
+
+import java.util.Date;
 
 @Controller
 public class WeatherController {
 
     @GetMapping("/weather")
-    public String getWeagfdrther(){
+    public String getWeather(){
         return "weather";
     }
 
     @PostMapping("/weather")
+    @ResponseBody
     public String postWeather(@RequestParam("city") String city){
         WeatherService weatherService = WeatherService.getInstance();
-        System.out.println(weatherService.getWeather(city));
-        return "weather";
+        //System.out.println(weatherService.getWeather(city));
+
+        String todayWeather = String.valueOf(weatherService.getWeather(city));
+
+
+        return new Date()+ " || "+todayWeather;
     }
 }
